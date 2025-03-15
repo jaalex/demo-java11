32d36cfed98ee7f38f2311814a80bd8457b303b3
Joseph Alex <jaalex@gmail.com>
2025-03-15 12:38:41 +0530

    add feature removed in java21

--- src/main/java/com/example/demojava11/DemoJava11Application.java
+++ src/main/java/com/example/demojava11/DemoJava11Application.java
@@ -1,5 +1,5 @@
 package com.example.demojava11;
-
+import java.lang.SecurityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoJava11Application {
 
 	public static void main(String[] args) {
+		SecurityManager sm = System.getSecurityManager();
+        if (sm == null) {
+            System.out.println("No SecurityManager installed.");
+        } else {
+            System.out.println("SecurityManager is active.");
+        }
 		SpringApplication.run(DemoJava11Application.class, args);
 	}
 }
